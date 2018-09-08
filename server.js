72ad6276e2a470b156cadbcc2e4aa94b4cdc9dc7
Abro1986 <AndrewBroestl@gmail.com>
2018-09-08 13:00:12 -0600

    changed the msg object

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 require('dotenv').config();
-//app.use(express.bodyParser());
 let bodyParser = require('body-parser')
+let express    = require('express');
+let app        = express();
+let cors       = require('cors');
+let router     = express.Router();
 
-let express = require('express');
-let app = express();
-var cors = require('cors');
 app.use(cors());
-const router = express.Router();
-let projectRouter = require('./config/routes.js')
-
 app.use(bodyParser.json());
 
 
@@ -18,14 +15,7 @@ app.use(bodyParser.json());
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 console.log(process.env.SENDGRID_API_KEY);
-const msg = {
-  to: 'AndrewBroestl@gmail.com',
-  from: 'AndrewBroestl@gmail.com',
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-};
-//sgMail.send(msg);
+
 
 app.post('/api/mail', function(req,res) {
 
@@ -33,15 +23,14 @@ app.post('/api/mail', function(req,res) {
 	console.log("hitting the mail post route")
 
 	let msgTwo = {
-  to: req.body.email,
-  from: 'AndrewBroestl@gmail.com',
+  to: 'AndrewBroestl@gmail.com',
+  from: req.body.email,
   subject: 'New email!',
   text: req.body.content,
   html: `<strong>${req.body.content}</strong>`,
 };
 
 	sgMail.send(msgTwo);
-console.log(req.body.content)
 	res.sendStatus(200)
 })
 
